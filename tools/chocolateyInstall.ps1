3996cb0c932f4cc9d5d1ce2817fd73487bdb1e8e
Patrick Hufford <pahuff@microsoft.com>
2017-01-25 13:37:18 -0800

    17.0.2 update
    new numbering scheme, but otherwise this is just the next version

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@
 $packageName = 'obs-studio';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)";
 $installerType = 'EXE';
-$url = 'https://github.com/jp9000/obs-studio/releases/download/0.16.5/OBS-Studio-0.16.5-Full-Installer.exe';
+$url = 'https://github.com/jp9000/obs-studio/releases/download/17.0.2/OBS-Studio-17.0.2-Full-Installer.exe';
 $url64 = $url;
 
 $packageArgs = @{
@@ -15,7 +15,7 @@ $packageArgs = @{
   silentArgs    = '/S'
   validExitCodes= @(0)
   softwareName  = 'obs-studio*'
-  checksum      = 'F7FB9A6CCA1A59356837C61F1BB9536EB1A0B45CA6FDA8D0185D9D7CEA82CBC3'
+  checksum      = '8AB9B029C9B48BB89ECBDB50A0B3F319'
   checksumType  = 'sha256'
 };
 
