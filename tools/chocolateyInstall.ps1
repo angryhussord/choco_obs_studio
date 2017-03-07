d77f729da459f27354ef8d3d33005a7d6309494b
Patrick Hufford <pahuff@microsoft.com>
2017-03-07 06:13:59 -0800

    18.0 update

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@
 $packageName = 'obs-studio';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)";
 $installerType = 'EXE';
-$url = 'https://github.com/jp9000/obs-studio/releases/download/17.0.2/OBS-Studio-17.0.2-Full-Installer.exe';
+$url = 'https://github.com/jp9000/obs-studio/releases/download/18.0.0/OBS-Studio-18.0-Full-Installer.exe';
 $url64 = $url;
 
 $packageArgs = @{
@@ -15,7 +15,7 @@ $packageArgs = @{
   silentArgs    = '/S'
   validExitCodes= @(0)
   softwareName  = 'obs-studio*'
-  checksum      = '8AB9B029C9B48BB89ECBDB50A0B3F319'
+  checksum      = '715A024816239CF38FC1809D0F9E9DCD'
   checksumType  = 'sha256'
 };
 
