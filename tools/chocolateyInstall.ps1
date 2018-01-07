5b9c36de3c411cbb44515872ea88efb103fc1634
Patrick Hufford <pahuff@microsoft.com>
2018-01-07 06:13:19 -0800

    20.1.3 update

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@
 $packageName = 'obs-studio';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)";
 $installerType = 'EXE';
-$url = 'https://github.com/jp9000/obs-studio/releases/download/20.1.1/OBS-Studio-20.1.1-Full-Installer.exe';
+$url = 'https://github.com/jp9000/obs-studio/releases/download/20.1.3/OBS-Studio-20.1.3-Full-Installer.exe';
 $url64 = $url;
 
 $packageArgs = @{
@@ -15,7 +15,7 @@ $packageArgs = @{
   silentArgs    = '/S'
   validExitCodes= @(0)
   softwareName  = 'obs-studio*'
-  checksum      = '77C1C2AAE93F97A153A6C8C6B98779A110B378F381B20340971EBF2EA8D98190'
+  checksum      = '6C185649C6472213F3067D9CB45023776030FDAD9B59C71060D711327981DCE0'
   checksumType  = 'sha256'
 };
 
